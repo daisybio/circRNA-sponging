6b4a3141fa6c8ddbd1a20b4054f16a37b311d7a9
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-11-07 19:08:54 +0100

    update

--- bin/differentalExpression.R
+++ bin/differentalExpression.R
@@ -12,7 +12,8 @@ args <- c("/Users/leonschwartz/Desktop/Bioinformatik/local_data/pipeline_expampl
           "/Users/leonschwartz/Desktop/Bioinformatik/local_data/pipeline_expample_output/subset.tsv",
           "hg38",
           "/Users/leonschwartz/Desktop/Bioinformatik/local_data/references/gencode.v35.primary_assembly.annotation.gtf",
-          "FALSE")
+          "FALSE",
+          "/Users/leonschwartz/Desktop/Bioinformatik/Bachelor/data/circRNAs/circRNA_annotated.tsv")
 # read sam file(s)
 sam_files <- list.files(args[1], pattern = "\\.sam$", full.names = TRUE, recursive = TRUE)
 
@@ -30,16 +31,43 @@ parse_boolean <- function(isT) {
     return (FALSE)
   }
 }
+# check for paired reads
 isPaired <- parse_boolean(args[5])
-# convert sam to countsFile
-
+# create ouptut data and plots
+create_outputs <- function(d, results, n, marker, file_name) {
+  # write data to disk
+  write.table(results, file = paste(file_name, "tsv", sep = "."), quote = FALSE, sep = "\t", col.names = NA)
+  # create normalized counts plots for top n genes
+  if (length(results@rownames) >= n) {
+    r_top = as.list(results@rownames)[1:n]
+    for (gene in r_top) {
+      pl = DESeq2::plotCounts(d, gene = gene, intgroup = marker)
+      gene_name = paste(file_name, gene, sep = "_")
+      png(filename = paste(gene_name, "png", sep = "."))
+      plot(pl)
+      dev.off
+    }
+  }
+  # PCA
+  vsdata <- DESeq2::vst(d, blind = FALSE)
+  PCA_plot <- DESeq2::plotPCA(vsdata, intgroup = marker)
+  plot_name <- paste(file_name, "pca", sep = "_")
+  png(filename = paste(plot_name, "png", sep = "."))
+  plot(PCA_plot)
+  dev.off
+}
+# convert sam to countsObject
 countsObject <- Rsubread::featureCounts(sam_files, annot.inbuilt = genome_version, annot.ext = gtf_file,
                             isGTFAnnotationFile = TRUE, isPairedEnd = isPaired)
 # prepare deseq2 counts data
 countsData <- countsObject[["counts"]]
-
-# write output file
-# write.table(countsData, file = "countsFile.tsv", quote = FALSE, sep = "\t", col.names = NA)
+# reformat ids
+new_names <- list()
+for (name in row.names(countsData)) {
+  general_name <- strsplit(name, "\\.")[[1]][1]
+  new_names <- c(new_names, general_name)
+}
+row.names(countsData) <- new_names
 
 # dds object
 dds <- DESeq2::DESeqDataSetFromMatrix(countData = countsData,
@@ -52,5 +80,17 @@ dds <- DESeq2::DESeq(dds)
 res <- DESeq2::results(dds)
 # sort by p-value
 res <- res[order(res$padj),]
-# write results to file
-write.table(res, file = "results.tsv", quote = FALSE, sep = "\t", col.names = NA)
+# create summary
+DESeq2::summary(res)
+
+# WRITE OUTPUTS
+# total_RNA
+create_outputs(d = dds, results = res, n = 5, marker = "condition", file_name = "total_rna")
+# only circRNA, if file loc is given
+if (length(args) == 6) {
+  circ_RNAs <- read.table(file = args[6], sep = "\t", header = TRUE)
+  ens_ids <- c_RNAs$Ensembl_gene_ID
+  # circ rnas only
+  filtered_res <- res[row.names(res) %in% ens_ids,]
+  create_outputs(d = dds, results = filtered_res, n = 5, marker = "condition", file_name = "circ_rna_only")
+}

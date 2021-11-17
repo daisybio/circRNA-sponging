d1451194b74729de3c1ea300ce34c61bb7abe049
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-11-17 17:41:47 +0100

    update

--- bin/circRNA_summarize_results.R
+++ bin/circRNA_summarize_results.R
@@ -26,15 +26,15 @@ for (i in 1:length(samples)){
   colnames(expression_raw) <- c("chr", "start", "stop", "strand", "counts", "gene_symbol", paste("isoform_", sample, sep = ""))
   expression_raw <- expression_raw[,c(1,2,3,4,5,6)]
   
-  # compact and remove duplicates, in case of duplicates, take max counts
-  compact_raw <- data.table(circRNA=paste0(strsplit(expression_raw$chr, "_")[[1]][1], ":", expression_raw$start, "-", expression_raw$stop,"_", expression_raw$strand),  gene_symbol = expression_raw$gene_symbol, counts = expression_raw$counts)
-  compact_raw_no_dup <- data.table(compact_raw[, max(counts), by=circRNA])
-  compact_raw <- merge(compact_raw, compact_raw_no_dup, by = "circRNA", all=FALSE)
+  # compact and remove duplicates
+  compact_raw <- data.table(circRNA=paste0(strsplit(expression_raw$chr, "_")[[1]][1], ":", expression_raw$start, "-", expression_raw$stop,"_", expression_raw$strand, "!", expression_raw$gene_symbol), counts = expression_raw$counts)
+  compact_raw <- compact_raw[, max(counts), by=circRNA]
 
   compact_raw$chr <- sapply(strsplit(as.character(compact_raw$circRNA),':'), "[", 1)
   compact_raw$start <- as.numeric(sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),':'), "[", 2),'-'), "[", 1))
   compact_raw$stop <- as.numeric(sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'-'), "[", 2),'_'), "[", 1))
-  compact_raw$strand <- sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2)
+  compact_raw$strand <- sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2), '!'), "[", 1)
+  compact_raw$gene_symbol <- sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2), '!'), "[", 2)
 
   expression <- compact_raw[,c("chr", "start", "stop", "strand", "gene_symbol", "V1")]
   colnames(expression)[6] <- sample

b5718337586022c42ebc8f0f45e34f544348110c
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-11-19 18:45:49 +0100

    update

--- bin/circRNA_summarize_results.R
+++ bin/circRNA_summarize_results.R
@@ -17,40 +17,33 @@ N_of_circRNAs_raw <- c()
 for (i in 1:length(samples)){
   sample <- samples[i]
   path <- paste0(output_dir,"/samples/", sample , "/circRNA_detection/circExplorer2/", sample ,"_circularRNA_known.txt")
-  
-  # Skip sample if file empty
-  if(file.size(path) == 0){
-    warning(paste0("Sample ", sample, " is empty (no circRNA detected). Sample will be removed from results"))
-    next
-  }
-  
   CIRCexplorer2_output <- data.table(read.table(path, sep = "\t"))
   
   N_of_circRNAs_raw[i] <- nrow(CIRCexplorer2_output)
   names(N_of_circRNAs_raw)[i] <- sample
   
   expression_raw <- CIRCexplorer2_output[,c(1,2,3,6,13,15,16)]
-  colnames(expression_raw) <- c("chr", "start", "stop", "strand", "counts", paste("gene_", sample, sep = ""), paste("isoform_", sample, sep = ""))
-  expression_raw <- expression_raw[,c(1,2,3,4,5)]
+  colnames(expression_raw) <- c("chr", "start", "stop", "strand", "counts", "gene_symbol", paste("isoform_", sample, sep = ""))
+  expression_raw <- expression_raw[,c(1,2,3,4,5,6)]
   
-  # compact and remove duplicates, in case of duplicates, take max counts
-  compact_raw <- data.table(circRNA=paste0(gsub("_", "", expression_raw$chr), ":", expression_raw$start, "-", expression_raw$stop,"_", expression_raw$strand),  counts = expression_raw$counts)
+  # compact and remove duplicates
+  compact_raw <- data.table(circRNA=paste0(strsplit(expression_raw$chr, "_")[[1]][1], ":", expression_raw$start, "-", expression_raw$stop,"_", expression_raw$strand, "!", expression_raw$gene_symbol), counts = expression_raw$counts)
   compact_raw <- compact_raw[, max(counts), by=circRNA]
   
   compact_raw$chr <- sapply(strsplit(as.character(compact_raw$circRNA),':'), "[", 1)
   compact_raw$start <- as.numeric(sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),':'), "[", 2),'-'), "[", 1))
   compact_raw$stop <- as.numeric(sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'-'), "[", 2),'_'), "[", 1))
-  compact_raw$strand <- sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2)
+  compact_raw$strand <- sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2), '!'), "[", 1)
+  compact_raw$gene_symbol <- sapply(strsplit(sapply(strsplit(as.character(compact_raw$circRNA),'_'), "[", 2), '!'), "[", 2)
   
-  expression <- compact_raw[,c("chr", "start", "stop", "strand", "V1")]
-  colnames(expression)[5] <- sample
-  print(expression)
+  expression <- compact_raw[,c("chr", "start", "stop", "strand", "gene_symbol", "V1")]
+  colnames(expression)[6] <- sample
   
   if(is.null(finaldata)){
     finaldata <- expression
   } else {
-    finaldata <- merge(finaldata, expression, by = c("chr", "start", "stop", "strand"), all = T)
+    finaldata <- merge(finaldata, expression, by = c("chr", "start", "stop", "strand", "gene_symbol"), all = T)
   }
 }
 finaldata[is.na(finaldata)] <- 0
-write.table(finaldata, paste(output_dir, "circRNA_counts_raw.tsv", sep = "/"), quote = F, sep = "\t", row.names = F)
+write.table(finaldata, paste0("circRNA_counts_raw.tsv"), quote = F, sep = "\t", row.names = F)

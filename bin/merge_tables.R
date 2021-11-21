318ed2e75c0db2f630c8956df1cb118ef76e18bc
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-11-21 14:17:46 +0100

    update

--- bin/merge_tables.R
+++ bin/merge_tables.R
@@ -5,6 +5,8 @@ args = commandArgs(trailingOnly = TRUE)
 sample_files <- list.files(path = args[1], full.names = T, pattern = "*_gene_expression\\.tsv$")
 out_file <- args[2]
 
+print(sample_files)
+
 total_expr <- data.frame()
 for (idx in seq_along(sample_files)) {
   file <- sample_files[idx]

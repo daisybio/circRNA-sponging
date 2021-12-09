bdba077b4d838a028a82b830f084f7fae8a2ba30
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-12-09 19:07:15 +0100

    update

--- bin/combine_expression.R
+++ bin/combine_expression.R
@@ -21,7 +21,7 @@ tx <- ensembldb::EnsDb(txdb)
 tx2gene <- ensembldb::transcripts(tx, return.type = "data.frame", columns = c("gene_name", "gene_id"))
 # failures <- rownames(tx2gene[is.na(tx2gene$gene_name),])
 # tx2gene[failures, "gene_name"] <- tx2gene[failures, "gene_id"]
-tx2gene <- tx2gene[, c("tx_id", "gene_id")]
+tx2gene <- tx2gene[, c("tx_id", "gene_name")]
 # txi object
 txi <- tximport::tximport(quant.files, type="salmon", tx2gene=tx2gene, ignoreTxVersion = T)
 # write total gene expression over samples to file

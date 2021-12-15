f6ec58d87393ef99ea61f0fbe4dc3f61925ff266
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2021-12-15 16:20:22 +0100

    update

--- bin/process_miranda_bp.R
+++ bin/process_miranda_bp.R
@@ -9,10 +9,11 @@ if (length(args)==2) {
   circ.annotation <- read.table(args[2], header = T, sep = "\t")
   circ.annotation.map <- data.table(Target=paste0(circ.annotation$chr, ":", circ.annotation$start, "-", circ.annotation$stop, "_", circ.annotation$strand),
                                     ID=circ.annotation$circRNA.ID)
-  miranda.bp$ID <- circ.annotation.map[match(miranda.bp$Target, circ.annotation.map$Target), "ID"]
+  miranda.bp$ID <- circ.annotation.map$ID[match(miranda.bp$Target, circ.annotation.map$Target)]
   f <- is.na(miranda.bp$ID)
   miranda.bp$ID[f] <- miranda.bp$Target[f]
   miranda.bp$Target <- miranda.bp$ID
+  miranda.bp$ID <- NULL
 }
 # create contigency table
 write.table(as.data.frame.matrix(table(miranda.bp$Target, miranda.bp$miRNA)), file = "miranda_counts_sponge.tsv", sep = "\t")

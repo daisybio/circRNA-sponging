d2d1a9df7f6c07d38ca53b12e4d2507da93a4f03
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-01-17 09:34:50 +0100

    update

--- bin/quantify_circ_expression.R
+++ bin/quantify_circ_expression.R
@@ -14,13 +14,35 @@ circ.counts <- read.table(args[3], header = T, sep = "\t")
 colnames(circ.counts) <- sapply(gsub("\\.", "-", colnames(circ.counts)), "[", 1)
 # create tmp
 dir.create("tmp", showWarnings = F)
+# determine annotation
+annotation <- "circBaseID" %in% colnames(circ.counts)
+# set row names
+if (annotation) {
+  rownames(circ.counts) <- circ.counts$circBaseID
+} else {
+  rownames(circ.counts) <- paste0(circ.counts$chr, ":", circ.counts$start, "-", circ.counts$stop, "_", circ.counts$strand)
+}
 
 # TODO: implement paired read option
 # run psirc for every sample
 for (i in 1:nrow(samplesheet)) {
   sample <- samplesheet[i, "sample"]
+  output <- file.path("tmp", sample)
   fastq <- samplesheet[i, "totalRNA1"]
-  cmd <- paste(c("psirc-quant quant -i", index, "-o", "./tmp", "--single", "-l", "15", "-s", "20", fastq), collapse = " ")
-  cat(cmd)
-  # system(cmd)
+  # write to tmp/sample
+  cmd <- paste(c("psirc-quant quant -i", index, "-o", output, "--single", "-l", "15", "-s", "20", fastq), collapse = " ")
+  system(cmd)           # invoke psirc command
+  # read created abundances
+  abundance <- read.table(file.path(output, "abundance.tsv"), sep = "\t", header = T)
+  if (annotation) {
+    abundance.circ <- abundance[grepl("circ", abundance$target_id),]
+  } else {
+    abundance.circ <- abundance[grepl("chr", abundance$target_id),]
+  }
+  # set counts to quantified levels
+  circ.counts[abundance.circ$target_id, sample] <- abundance.circ$est_counts
+  cat(eval(round(i/nrow(samplesheet), 2)*100), " %", "\r")
 }
+# write output to disk
+o <- paste0(strsplit(basename(args[3]), "\\.")[[1]][1], "_quant", ".tsv")
+write.table(circ.counts, file = o, sep = "\t")

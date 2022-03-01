d99806a46642bc0a4d6fd8f1418a7f20fdc425a2
Leon Schwartz <leonschwartz@h034.tum.vpn.lrz.de>
2022-03-01 17:50:17 +0100

    update

--- bin/SPONGE_analysis.R
+++ bin/SPONGE_analysis.R
@@ -30,13 +30,16 @@ while(not_done)
 print("SUCCESS")
 
 # get all differentially expressed circRNAs that act as ceRNAs
-differential.circ.in.ce.network.A <- merge(ceRNA_interactions_all_circ, signif.hits, by.x = "geneA", by.y = "X")
-differential.circ.in.ce.network.B <- merge(ceRNA_interactions_all_circ, signif.hits, by.x = "geneB", by.y = "X")
-differential.circ.in.ce.network <- merge(differential.circ.in.ce.network.A, differential.circ.in.ce.network.B, all = T)
-
+# build network for all DE circRNAs
+de.circ.network <- c()
+for (hit in signif.hits$X) {
+  circ.network <- subnetwork(ceRNA_interactions_all_circ, pattern = hit)
+  de.circ.network <- rbind(de.circ.network, circ.network)
+}
+differential.circ.in.ce.network <- de.circ.network
 # find gene symbols for ensg
-targets <- c(differential.circ.in.ce.network$geneA, differential.circ.in.ce.network$geneB)
-targets <- unique(targets[!grepl(ifelse(annotation, "circ", "chr"), targets)])
+targets <- unique(c(differential.circ.in.ce.network$geneA, differential.circ.in.ce.network$geneB))
+targets <- targets[!grepl(ifelse(annotation, "circ", "chr"), targets)]
 # start query
 gene.ens.all <- biomaRt::getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
                                filter = "ensembl_gene_id",
@@ -48,27 +51,25 @@ differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.e
 differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol),"geneA"] <- differential.circ.in.ce.network$hgnc_symbol[!is.na(differential.circ.in.ce.network$hgnc_symbol)]
 differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.ens.all, by.x = "geneB", by.y = 1, all.x = T)
 differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol.y),"geneB"] <- differential.circ.in.ce.network$hgnc_symbol.y[!is.na(differential.circ.in.ce.network$hgnc_symbol.y)]
-# build network for all DE circRNAs
-de.circ.network <- c()
-for (hit in signif.hits$X) {
-  circ.network <- subnetwork(ceRNA_interactions_all_circ, pattern = hit)
-  de.circ.network <- rbind(de.circ.network, circ.network)
-}
-differential.circ.in.ce.network <- de.circ.network
 
 differential.circ.plot <- sponge_plot_network(differential.circ.in.ce.network, genes_miRNA_candidates, ) %>%
   visNetwork::visEdges(arrows = list(to = list(enabled = T, scaleFactor = 1)))
 differential.circ.plot$x$edges$label <- paste("mscor:", round(differential.circ.in.ce.network$mscor, 2))
 differential.circ.plot
 
+hgncs <- merge(signif.hits, gene.ens.all, by.x = "X", by.y = "ensembl_gene_id")
 nodes <- differential.circ.plot$x$nodes
 nodes$color <- NULL
-nodes$shape <- NULL
+nodes[nodes$id %in% hgncs$hgnc_symbol | nodes$id %in% signif.hits$X,"color"] <- "#CC3333"
+nodes$shape <- ifelse(grepl("circ", nodes$id), "rectangle", "triangle")
 nodes$group <- ifelse(grepl("circ", nodes$id), "circRNA", "mRNA")
+nodes[nodes$id %in% hgncs$hgnc_symbol | nodes$id %in% signif.hits$X,"group"] <- "DE"
+
 test <- visNetwork(nodes = nodes, edges = differential.circ.plot$x$edges) %>%
   visIgraphLayout(type = "full", physics = F) %>%
   visEdges(arrows = list(to = list(enabled = T, scaleFactor = 1))) %>%
   visGroups(groupname = "circRNA", shape = "rectangle", color = "#33FF99") %>%
   visGroups(groupname = "mRNA", shape = "triangle", color = "#0066CC") %>% 
+  visGroups(groupname = "DE", color = "#CC3333", shape = "rectangle") %>%
   visLegend()
 test

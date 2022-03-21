4e7aac2685e08bdb74afa206f54cf87797baa9af
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-03-21 13:52:13 +0100

    update

--- bin/compute_correlations.R
+++ bin/compute_correlations.R
@@ -103,11 +103,13 @@ miRNA_for_row <- function(miRNA_expr_line, circRNA, circRNA_counts){
   return(res)
 }
 
-# TODO: handle annotation!!!
 circRNA_for_row <- function(circRNA_expr_line){
   # get coordinations of current circRNA
   if (annotation) {
     circRNA <- circRNA_expr_line[8]
+    circRNA <- ifelse(circRNA_expr_line$circBaseID != "None", 
+                                  circRNA_expr_line[8], 
+                                  paste0(circRNA_expr_line[1], ":", circRNA_expr_line[2], ":", circRNA_expr_line[3], ":", circRNA_expr_line[4]))
     circRNA_counts <- circRNA_expr_line[-c(1:8)]
   } else {
     chr <- as.character(circRNA_expr_line[1])

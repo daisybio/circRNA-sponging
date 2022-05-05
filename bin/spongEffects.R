f749669ab48c72f30ffca3eb10a30e114fd57348
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-05-05 11:22:01 +0200

    update

--- bin/spongEffects.R
+++ bin/spongEffects.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-install.packages("pacman")
+install.packages("pacman", repos = "http://cran.us.r-project.org")
 pacman::p_load(SPONGE, doParallel, foreach, dplyr, argparser)
 
 args = commandArgs(trailingOnly = TRUE)

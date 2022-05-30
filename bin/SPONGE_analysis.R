7fbf71b7cffac8dce803d31422a16e0d6c17aecd
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-05-30 19:41:19 +0200

    update

--- bin/SPONGE_analysis.R
+++ bin/SPONGE_analysis.R
@@ -2,6 +2,7 @@
 
 library(SPONGE)
 library(visNetwork)
+library(MetBrewer)
 
 args = commandArgs(trailingOnly = TRUE)
 
@@ -20,11 +21,6 @@ gtf <- rtracklayer::readGFF(gtf)
 gene.ens.all <- unique(gtf[!is.na(gtf$transcript_id),c("gene_id", "gene_name", "gene_biotype")])
 colnames(gene.ens.all) <- c("ensembl_gene_id", "hgnc_symbol", "gene_biotype")
 rownames(gene.ens.all) <- gene.ens.all$gene_id
-# merge for geneA and geneB
-differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.ens.all, by.x = "geneA", by.y = 1, all.x = T)
-differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol),"geneA"] <- differential.circ.in.ce.network$hgnc_symbol[!is.na(differential.circ.in.ce.network$hgnc_symbol)]
-differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.ens.all, by.x = "geneB", by.y = 1, all.x = T)
-differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol.y),"geneB"] <- differential.circ.in.ce.network$hgnc_symbol.y[!is.na(differential.circ.in.ce.network$hgnc_symbol.y)]
 
 differential.circ.plot <- sponge_plot_network(differential.circ.in.ce.network, genes_miRNA_candidates, ) %>%
   visNetwork::visEdges(arrows = list(to = list(enabled = T, scaleFactor = 1)))
@@ -32,10 +28,20 @@ differential.circ.plot$x$edges$label <- paste("mscor:", round(differential.circ.
 
 hgncs <- merge(signif.hits, gene.ens.all, by.x = "X", by.y = "ensembl_gene_id")
 nodes <- differential.circ.plot$x$nodes
+nodes <- merge(nodes, gene.ens.all, by = 1, all.x = T)
+# change to hgnc
+nodes[!is.na(nodes$hgnc_symbol),"id"] <- nodes[!is.na(nodes$hgnc_symbol),"hgnc_symbol"]
+# add circRNA as biotype
+nodes[is.na(nodes$gene_biotype),"gene_biotype"] <- "circRNA"
+biotypes <- unique(nodes$gene_biotype)
+style <- data.frame(groupname=biotypes, 
+                    shape=seq(1,length(biotypes)), 
+                    color = as.vector(met.brewer("Juarez", length(biotypes))))
 nodes$color <- NULL
 nodes[nodes$id %in% hgncs$hgnc_symbol | nodes$id %in% signif.hits$X,"color"] <- "#CC3333"
 nodes$shape <- ifelse(grepl("c", nodes$id), "rectangle", "triangle")
 nodes$group <- ifelse(grepl("c", nodes$id), "circRNA", "mRNA")
+nodes$group[grep("c", nodes$id)] <- "circRNA"
 nodes[nodes$id %in% hgncs$hgnc_symbol | nodes$id %in% signif.hits$X,"group"] <- "DE"
 
 graph <- visNetwork(nodes = nodes, edges = differential.circ.plot$x$edges) %>%

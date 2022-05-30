7957cf3a6aa63e89ada6af9314370bf9065b23fb
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-05-30 20:50:10 +0200

    update

--- bin/SPONGE_analysis.R
+++ bin/SPONGE_analysis.R
@@ -27,7 +27,7 @@ differential.circ.plot <- sponge_plot_network(differential.circ.in.ce.network, g
 differential.circ.plot$x$edges$label <- paste("mscor:", round(differential.circ.in.ce.network$mscor, 2))
 
 # break types of RNA down to lncRNA and coding
-gene.ens.all$gene_biotype[gene.ens.all$gene_biotype != "protein_coding" & gene.ens.all$gene_biotype != "lncRNA"] <- "RNA"
+gene.ens.all$gene_biotype[gene.ens.all$gene_biotype != "protein_coding" & gene.ens.all$gene_biotype != "lncRNA"] <- "other_RNA"
 
 hgncs <- merge(signif.hits, gene.ens.all, by.x = "X", by.y = "ensembl_gene_id")
 nodes <- differential.circ.plot$x$nodes
@@ -46,7 +46,7 @@ nodes <- nodes[,-c(3,4)]
 # change to group
 colnames(nodes)[8] <- "group"
 # add new shape and color
-nodes <- merge(nodes, style, by.x = "group", by.y = "groupname", all.x = T)
+# nodes <- merge(nodes, style, by.x = "group", by.y = "groupname", all.x = T)
 # mark differentially expressed RNAs
 nodes[nodes$id %in% hgncs$hgnc_symbol | nodes$id %in% signif.hits$X,"group"] <- "DE"
 # change edges
@@ -60,8 +60,10 @@ edges[!is.na(edges$hgnc_symbol.y),"to"] <- edges$hgnc_symbol.y[!is.na(edges$hgnc
 graph <- visNetwork(nodes = nodes, edges = edges) %>%
   visIgraphLayout(type = "full", physics = F) %>%
   visGroups(groupname = "circRNA", shape = "rectangle", color = "#33FF99") %>%
-  visGroups(groupname = "mRNA", shape = "triangle", color = "#0066CC") %>% 
-  visGroups(groupname = "DE", color = "#CC3333", shape = "rectangle") %>%
+  visGroups(groupname = "protein_coding", shape = "triangle", color = "#0066CC") %>%
+  visGroups(groupname = "lncRNA", shape = "square", color = "#F8766D") %>%
+  visGroups(groupname = "other_RNA", color = "#DC71FA", shape = "diamond") %>%
+  visGroups(groupname = "DE", color = "#CC3333") %>%
   visLegend()
 graph
 

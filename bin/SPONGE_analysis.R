fea9d8e4aefa057b570e22f2a7e486ef1c17f378
Leon Schwartz <leonschwartz@MacBook-Pro-von-Leon.local>
2022-05-30 19:58:21 +0200

    update

--- bin/SPONGE_analysis.R
+++ bin/SPONGE_analysis.R
@@ -21,6 +21,11 @@ gtf <- rtracklayer::readGFF(gtf)
 gene.ens.all <- unique(gtf[!is.na(gtf$transcript_id),c("gene_id", "gene_name", "gene_biotype")])
 colnames(gene.ens.all) <- c("ensembl_gene_id", "hgnc_symbol", "gene_biotype")
 rownames(gene.ens.all) <- gene.ens.all$gene_id
+# convert geneA and geneB
+differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.ens.all, by.x = "geneA", by.y = 1, all.x = T)
+differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol),"geneA"] <- differential.circ.in.ce.network$hgnc_symbol[!is.na(differential.circ.in.ce.network$hgnc_symbol)]
+differential.circ.in.ce.network <- merge(differential.circ.in.ce.network, gene.ens.all, by.x = "geneB", by.y = 1, all.x = T)
+differential.circ.in.ce.network[!is.na(differential.circ.in.ce.network$hgnc_symbol.y),"geneB"] <- differential.circ.in.ce.network$hgnc_symbol.y[!is.na(differential.circ.in.ce.network$hgnc_symbol.y)]
 
 differential.circ.plot <- sponge_plot_network(differential.circ.in.ce.network, genes_miRNA_candidates, ) %>%
   visNetwork::visEdges(arrows = list(to = list(enabled = T, scaleFactor = 1)))
@@ -28,9 +33,8 @@ differential.circ.plot$x$edges$label <- paste("mscor:", round(differential.circ.
 
 hgncs <- merge(signif.hits, gene.ens.all, by.x = "X", by.y = "ensembl_gene_id")
 nodes <- differential.circ.plot$x$nodes
-nodes <- merge(nodes, gene.ens.all, by = 1, all.x = T)
-# change to hgnc
-nodes[!is.na(nodes$hgnc_symbol),1:2] <- nodes[!is.na(nodes$hgnc_symbol),"hgnc_symbol"]
+nodes <- merge(nodes, gene.ens.all[,c("ensembl_gene_id", "gene_biotype")], by = 1, all.x = T)
+
 # add circRNA as biotype
 nodes[is.na(nodes$gene_biotype),"gene_biotype"] <- "circRNA"
 biotypes <- unique(nodes$gene_biotype)
